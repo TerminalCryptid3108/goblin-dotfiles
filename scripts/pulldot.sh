2589ed50c2056e71510a378ab99008d003c5ddc6
TerminalCryptid3108 <sebgamer@live.dk>
2026-05-25 21:52:52 +0200

    added new goblinry

--- scripts/pulldot.sh
+++ scripts/pulldot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd ~/dotfiles || exit
 
-if git pull, then
+if git pull; then
 echo ""
 echo "acquired new goblinry!"
 fi
 
-cp -r ~/dotfiles/images ~/Pictures/
+cp -r ~/dotfiles/ascii /home/seb/Pictures/
 cp -r ~/dotfiles/sounds ~/
 cp -r ~/dotfiles/scripts ~/
-cp ~/dotfiles/.bashrc /.bashrc
-cp ~/dotfiles/.bash_aliases ~/.bash_aliases
+cp ~/dotfiles/.bashrc /
+cp ~/dotfiles/.bash_aliases ~/
