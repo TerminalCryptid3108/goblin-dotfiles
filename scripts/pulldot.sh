acdacee966ca152dda3a7054de1de3136738faba
TerminalCryptid3108 <sebgamer@live.dk>
2026-05-25 22:21:30 +0200

    added new goblinry

--- scripts/pulldot.sh
+++ scripts/pulldot.sh
@@ -9,6 +9,6 @@ fi
 cp -r ~/dotfiles/ascii /home/seb/Pictures/
 cp -r ~/dotfiles/sounds ~/
 cp -r ~/dotfiles/scripts ~/
-cp ~/dotfiles/.bashrc /
+cp ~/dotfiles/.bashrc ~/
 cp ~/dotfiles/.bash_aliases ~/
 cp ~/dotfiles/neofetch/config.conf ~/.config/neofetch/
